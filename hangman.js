61e916f653edfac0d7e3aa0a7066e8a53772556a
Allxie <allxiecleary@gmail.com>
2015-05-20 10:56:30 -0700

    with hangman drawing working

--- hangman.js
+++ hangman.js
@@ -3,12 +3,7 @@ var HangManCtrl = function($scope) {
   $scope.hiddenCharls = [];
   $scope.win = " ";
   $scope.lose = " ";
-  $scope.rArm = " ";
-  $scope.lArm = " ";
-  $scope.head = " ";
-  $scope.bod = " ";
-  $scope.rLeg = " ";
-  $scope.lLeg = " ";
+  $("#hangDraw").html("<br>&nbsp;&nbsp;____<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_|_<br>&nbsp;&nbsp;&nbsp;&nbsp;|___|");
 
 
   $scope.addWord = function(){
@@ -27,10 +22,40 @@ var HangManCtrl = function($scope) {
   $scope.guessLetter = function(){
   	if($scope.guesses.indexOf($scope.letter) === -1 && $scope.currentWord.indexOf($scope.letter) === -1) {
   		$scope.guesses.push($scope.letter);
-  		if($scope.guesses.length = 6){
-  			$scope.lose = "You LOSE. And you're DEAD.";
-  		}
-  	}
+
+      switch ($scope.guesses.length) {
+      case 0:
+
+          break;
+      case 1:
+          $("#hangDraw").html("<br>&nbsp;&nbsp;____<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;0&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_|_<br>&nbsp;&nbsp;&nbsp;&nbsp;|___|");
+          //$("#head").html("0");
+          break;
+      case 2:
+          $("#hangDraw").html("<br>&nbsp;&nbsp;____<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>\\0&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_|_<br>&nbsp;&nbsp;&nbsp;&nbsp;|___|");
+
+          //$("#rArm").html("/");
+          break;
+      case 3:
+          $("#hangDraw").html("<br>&nbsp;&nbsp;____<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>\\0/&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_|_<br>&nbsp;&nbsp;&nbsp;&nbsp;|___|");
+          
+          //$("#lArm").html("\\");
+          break;
+      case 4:
+          $("#hangDraw").html("<br>&nbsp;&nbsp;____<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>\\0/&nbsp;&nbsp;&nbsp;|<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_|_<br>&nbsp;&nbsp;&nbsp;&nbsp;|___|");
+          //$("#bod").html("|");
+          break;
+      case 5:
+          $("#hangDraw").html("<br>&nbsp;&nbsp;____<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>\\0/&nbsp;&nbsp;&nbsp;|<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>/&nbsp;&nbsp;&nbsp;&nbsp;_|_<br>&nbsp;&nbsp;&nbsp;&nbsp;|___|");
+
+          //$("#lLeg").html("/");
+          break;
+      case 6:
+          $("#hangDraw").html("<br>&nbsp;&nbsp;____<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>\\0/&nbsp;&nbsp;&nbsp;|<br>&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|<br>/&nbsp;\\&nbsp;&nbsp;_|_<br>&nbsp;&nbsp;&nbsp;&nbsp;|___|");
+          $scope.lose = "You LOSE. And you're DEAD.";
+          break;
+      }
+    }
 
 		indOfGuessed = $scope.currentWord.indexOf($scope.letter)
 		if ( indOfGuessed!= -1) {
